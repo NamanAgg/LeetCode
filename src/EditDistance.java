a3d6e902967a934d3b5e488be21c83192db83b62
naman <namanagg.2010@gmail.com>
2021-07-06 23:52:24 +0530

    update

---
+++ src/EditDistance.java
@@ -0,0 +1,81 @@
+//********************72. Edit Distance
+// Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+// You have the following three operations permitted on a word:
+// Insert a character
+// Delete a character
+// Replace a character
+
+// Example 1:
+// Input: word1 = "horse", word2 = "ros"
+// Output: 3
+// Explanation: 
+// horse -> rorse (replace 'h' with 'r')
+// rorse -> rose (remove 'r')
+// rose -> ros (remove 'e')
+
+// Example 2:
+// Input: word1 = "intention", word2 = "execution"
+// Output: 5
+// Explanation: 
+// intention -> inention (remove 't')
+// inention -> enention (replace 'i' with 'e')
+// enention -> exention (replace 'n' with 'x')
+// exention -> exection (replace 'n' with 'c')
+// exection -> execution (insert 'u')
+
+// Constraints:
+// 0 <= word1.length, word2.length <= 500
+// word1 and word2 consist of lowercase English letters.
+public class EditDistance {
+    public int minDistance(String s1, String s2, int n, int m, int[][] dp) {
+        if (n == 0 || m == 0) {
+            return dp[n][m] = (n == 0 ? m : n);
+        }
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        int insert = minDistance(s1, s2, n, m - 1, dp);
+        int replace = minDistance(s1, s2, n - 1, m - 1, dp);
+        int delete = minDistance(s1, s2, n - 1, m, dp);
+
+        if (s1.charAt(n - 1) == s2.charAt(m - 1))
+            return dp[n][m] = replace;
+        else
+            return dp[n][m] = Math.min(Math.min(insert, replace), delete) + 1;
+    }
+
+    public int minDistance_DP(String s1, String s2, int N, int M, int[][] dp) {
+
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (n == 0 || m == 0) {
+                    dp[n][m] = (n == 0 ? m : n);
+                    continue;
+                }
+
+                int insert = dp[n][m - 1];// minDistance(s1, s2, n, m - 1, dp);
+                int replace = dp[n - 1][m - 1];// minDistance(s1, s2, n - 1, m - 1, dp);
+                int delete = dp[n - 1][m];// minDistance(s1, s2, n - 1, m, dp);
+
+                if (s1.charAt(n - 1) == s2.charAt(m - 1))
+                    dp[n][m] = replace;
+                else
+                    dp[n][m] = Math.min(Math.min(insert, replace), delete) + 1;
+            }
+        }
+
+        return dp[N][M];
+    }
+
+    public int minDistance(String word1, String word2) {
+        int n = word1.length(), m = word2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        int ans = minDistance_DP(word1, word2, n, m, dp);
+        return ans;
+    }
+}
