dd89a302e8c9f25b39e0fd751595506c6406901d
naman <namanagg.2010@gmail.com>
2021-07-13 14:48:08 +0530

    update

---
+++ src/RottingOranges.java
@@ -0,0 +1,70 @@
+//***************994. Rotting Oranges
+// You are given an m x n grid where each cell can have one of three values:
+// 0 representing an empty cell,
+// 1 representing a fresh orange, or
+// 2 representing a rotten orange.
+// Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+// Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+
+// Example 1:
+// Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+// Output: 4
+
+// Example 2:
+// Input: grid = [[2,1,1],[0,1,1],[1,0,1]]
+// Output: -1
+// Explanation: The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+
+// Example 3:
+// Input: grid = [[0,2]]
+// Output: 0
+// Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
+
+// Constraints:
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 10
+// grid[i][j] is 0, 1, or 2.
+public class RottingOranges {
+    public int orangesRotting(int[][] grid) {
+        LinkedList<Integer> que = new LinkedList<>();
+        int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+        int freshOranges = 0, time = 0, n = grid.length, m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1)
+                    freshOranges++;
+                else if (grid[i][j] == 2) {
+                    que.addLast(i * m + j);
+                    grid[i][j] = 2; // mark them visited
+                }
+            }
+        }
+
+        if (freshOranges == 0)
+            return 0;
+        while (que.size() != 0) {
+            int size = que.size();
+            while (size-- > 0) {
+                int rottedOrangeIDX = que.removeFirst();
+                int sr = rottedOrangeIDX / m, sc = rottedOrangeIDX % m;
+                for (int d = 0; d < 4; d++) {
+                    int r = sr + dir[d][0];
+                    int c = sc + dir[d][1];
+
+                    if (r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 1) {
+                        if (--freshOranges == 0)
+                            return time + 1;
+                        grid[r][c] = 2;
+                        que.addLast(r * m + c);
+                    }
+                }
+            }
+            time++;
+        }
+
+        return -1;
+    }
+    
+}
