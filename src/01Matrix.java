6e43230b8d017392b0125de5ea6a732993dc0e62
naman <namanagg.2010@gmail.com>
2021-07-15 02:27:15 +0530

    update

---
+++ src/01Matrix.java
@@ -0,0 +1,61 @@
+//************542. 01 Matrix
+// Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+// The distance between two adjacent cells is 1.
+
+// Example 1:
+// Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+// Output: [[0,0,0],[0,1,0],[0,0,0]]
+
+// Example 2:
+// Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+// Output: [[0,0,0],[0,1,0],[1,2,1]]
+
+// Constraints:
+// m == mat.length
+// n == mat[i].length
+// 1 <= m, n <= 104
+// 1 <= m * n <= 104
+// mat[i][j] is either 0 or 1.
+// There is at least one 0 in mat.
+public class 01Matrix {
+      public int[][] updateMatrix(int[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0)
+            return grid;
+
+        int n = grid.length, m = grid[0].length;
+        boolean[][] vis = new boolean[n][m];
+
+        LinkedList<Integer> que = new LinkedList<>();
+        int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 0) {
+                    vis[i][j] = true;
+                    que.addLast(i * m + j);
+                }
+            }
+        }
+
+        while (que.size() != 0) {
+            int size = que.size();
+            while (size-- > 0) {
+                int idx = que.removeFirst();
+                int sr = idx / m, sc = idx % m;
+
+                for (int d = 0; d < dir.length; d++) {
+                    int r = sr + dir[d][0];
+                    int c = sc + dir[d][1];
+
+                    if (r >= 0 && c >= 0 && r < n && c < m && !vis[r][c]) {
+                        vis[r][c] = true;
+                        grid[r][c] = grid[sr][sc] + 1;
+                        que.addLast(r * m + c);
+                    }
+                }
+            }
+        }
+
+        return grid;
+    }
+}
