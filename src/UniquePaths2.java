4f986034ba320d8353ac7d36d59cca70fa7e4f78
NamanAgg <51829777+NamanAgg@users.noreply.github.com>
2021-07-17 01:25:29 +0530

    update

---
+++ src/UniquePaths2.java
@@ -0,0 +1,60 @@
+//*************63. Unique Paths II
+// A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+// The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+// Now consider if some obstacles are added to the grids. How many unique paths would there be?
+// An obstacle and space is marked as 1 and 0 respectively in the grid.
+
+// Example 1:
+// Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+// Output: 2
+// Explanation: There is one obstacle in the middle of the 3x3 grid above.
+// There are two ways to reach the bottom-right corner:
+// 1. Right -> Right -> Down -> Down
+// 2. Down -> Down -> Right -> Right
+
+// Example 2:
+// Input: obstacleGrid = [[0,1],[0,0]]
+// Output: 1
+
+// Constraints:
+// m == obstacleGrid.length
+// n == obstacleGrid[i].length
+// 1 <= m, n <= 100
+// obstacleGrid[i][j] is 0 or 1.
+public class UniquePaths2{
+    public int mazePath_HDV_DP(int SR, int SC, int er, int ec, int[][] dir, int[][] dp, int[][] obstacleGrid) {
+
+        for (int sr = er; sr >= 0; sr--) {
+            for (int sc = ec; sc >= 0; sc--) {
+                if (sr == er && sc == ec) {
+                    dp[sr][sc] = 1;
+                    continue;
+                }
+
+                int count = 0;
+                for (int d = 0; d < dir.length; d++) {
+                    int r = sr + dir[d][0];
+                    int c = sc + dir[d][1];
+                    if (r >= 0 && c >= 0 && r <= er && c <= ec && obstacleGrid[r][c] == 0) {
+                        count += dp[r][c];// mazePath_HDV(r, c, er, ec, dir, dp);
+                    }
+                }
+
+                dp[sr][sc] = count;
+            }
+        }
+
+        return dp[SR][SC];
+    }
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[n - 1][m - 1] == 1)
+            return 0;
+
+        int[][] dp = new int[n][m];
+        int[][] dir = { { 1, 0 }, { 0, 1 } };
+
+        return mazePath_HDV_DP(0, 0, n - 1, m - 1, dir, dp, obstacleGrid);
+    }
+}
