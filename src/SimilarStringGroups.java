2d8719f15733d7986d473951c5a33608cd56eedf
nmn <namanagg.2010@gmail.com>
2021-07-21 01:23:17 +0530

    commit

--- src/SimilarStringGroups.java
+++ src/SimilarStringGroups.java
@@ -21,5 +21,42 @@
 // strs[i] consists of lowercase letters only.
 // All words in strs have the same length and are anagrams of each other.
 public class SimilarStringGroups{
+    int[] par, size;
 
+    public int findPar(int u) {
+        return par[u] == u ? u : (par[u] = findPar(par[u]));
+    }
+    
+    public boolean isSimilar(String s1, String s2) {
+        int count = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i) && ++count > 2)
+                return false;
+        }
+
+        return true;
+    }
+
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length, group = n;
+        par = new int[n];
+        for (int i = 0; i < n; i++)
+            par[i] = i;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isSimilar(strs[i], strs[j])) {
+                    int p1 = findPar(i);
+                    int p2 = findPar(j);
+
+                    if (p1 != p2) {
+                        par[p1] = p2;
+                        group--;
+                    }
+                }
+            }
+        }
+
+        return group;
+    }
 }
