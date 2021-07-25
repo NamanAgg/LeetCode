c7bfd2666fe5f8ece123562c1f441da5c708c69c
nmn <namanagg.2010@gmail.com>
2021-07-25 21:13:11 +0530

    commit

---
+++ src/BusRoutes.java
@@ -0,0 +1,73 @@
+//*****************815. Bus Routes
+// You are given an array routes representing bus routes where routes[i] is a bus route that the ith bus repeats forever.
+
+// For example, if routes[0] = [1, 5, 7],
+// this means that the 0th bus travels in the sequence 1 -> 5 -> 7 -> 1 -> 5 -> 7 -> 1 -> ... forever.
+// You will start at the bus stop source (You are not on any bus initially), and you want to go to the bus stop target.
+// You can travel between bus stops by buses only.
+// Return the least number of buses you must take to travel from source to target. Return -1 if it is not possible.
+
+// Example 1:
+// Input: routes = [[1,2,7],[3,6,7]], source = 1, target = 6
+// Output: 2
+// Explanation: The best strategy is take the first bus to the bus stop 7, then take the second bus to the bus stop 6.
+
+// Example 2:
+// Input: routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]], source = 15, target = 12
+// Output: -1
+ 
+// Constraints:
+// 1 <= routes.length <= 500.
+// 1 <= routes[i].length <= 105
+// All the values of routes[i] are unique.
+// sum(routes[i].length) <= 105
+// 0 <= routes[i][j] < 106
+// 0 <= source, target < 106
+public class BusRoutes{
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        if(source==target) return 0;
+        int N = routes.length;
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>(); // busStand to bus mapping
+        for (int bus = 0; bus < routes.length; bus++) {
+            for (int busStand : routes[bus]) {
+                map.putIfAbsent(busStand, new ArrayList<>());
+                map.get(busStand).add(bus);
+            }
+        }
+
+        HashSet<Integer> busStandVisted = new HashSet<>();
+        boolean[] busVisited = new boolean[N];
+        int interchange = 0;
+
+        LinkedList<Integer> que = new LinkedList<>();
+        que.addLast(source);
+        busStandVisted.add(source);
+
+        while (que.size() != 0) {
+            int size = que.size();
+            while (size-- > 0) {
+                int busStand = que.removeFirst();
+                for (int bus : map.get(busStand)) {
+
+                    if (busVisited[bus])
+                        continue;
+
+                    for (int upcomingBusStand : routes[bus]) {
+                        if (!busStandVisted.contains(upcomingBusStand)) {
+                            busStandVisted.add(upcomingBusStand);
+                            que.addLast(upcomingBusStand);
+                            if (upcomingBusStand == target) {
+                                return interchange + 1;
+                            }
+                        }
+                    }
+
+                    busVisited[bus] = true;
+                }
+            }
+            interchange++;
+        }
+
+        return -1;
+    }
+}
---
+++ src/RegionsCutBySlashes.java
@@ -0,0 +1,75 @@
+//*****************959. Regions Cut By Slashes
+// An n x n grid is composed of 1 x 1 squares where each 1 x 1 square consists of a '/', '\', or blank space ' '.
+// These characters divide the square into contiguous regions.
+// Given the grid grid represented as a string array, return the number of regions.
+// Note that backslash characters are escaped, so a '\' is represented as '\\'.
+
+// Example 1:
+// Input: grid = [" /","/ "]
+// Output: 2
+
+// Example 2:
+// Input: grid = [" /","  "]
+// Output: 1
+
+// Example 3:
+// Input: grid = ["\\/","/\\"]
+// Output: 4
+// Explanation: (Recall that because \ characters are escaped, "\\/" refers to \/, and "/\\" refers to /\.)
+
+// Example 4:
+// Input: grid = ["/\\","\\/"]
+// Output: 5
+// Explanation: (Recall that because \ characters are escaped, "\\/" refers to \/, and "/\\" refers to /\.)
+
+// Example 5:
+// Input: grid = ["//","/ "]
+// Output: 3
+ 
+// Constraints:
+// n == grid.length
+// n == grid[i].length
+// 1 <= n <= 30
+// grid[i][j] is either '/', '\', or ' '.
+public class RegionsCutBySlashes{
+    int[] par, size;
+
+    public int findPar(int u) {
+        return par[u] == u ? u : (par[u] = findPar(par[u]));
+    }
+    public int regionsBySlashes(String[] grid) {
+        if (grid.length == 0)
+            return 0;
+        int n = grid.length, m = n + 1, region = 1;
+        par = new int[m * m];
+        for (int i = 0; i < m * m; i++) {
+            par[i] = i;
+            int r = i / m, c = i % m;
+            if (r == 0 || c == 0 || r == m - 1 || c == m - 1)
+                par[i] = 0;
+        }
+
+        for (int i = 0; i < n; i++) {
+            String s = grid[i];
+            for (int j = 0; j < s.length(); j++) {
+                char ch = s.charAt(j);
+                int p1 = -10000, p2 = -100000;
+                if (ch == '/') {
+                    p1 = findPar(i * m + j + 1);
+                    p2 = findPar((i + 1) * m + j);
+                } else if (ch == '\\') {
+                    p1 = findPar(i * m + j);
+                    p2 = findPar((i + 1) * m + j + 1);
+                } else // for space
+                    continue;
+
+                if (p1 != p2)
+                    par[p1] = p2;
+                else
+                    region++;
+            }
+        }
+
+        return region;
+    }
+}
