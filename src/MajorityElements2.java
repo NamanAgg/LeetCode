2dd479afa3e12524eb15cbd5a5d3a7c0f66c0674
nmn <namanagg.2010@gmail.com>
2021-09-10 23:17:31 +0530

    commit

---
+++ src/MajorityElements2.java
@@ -0,0 +1,67 @@
+//***********229. Majority Element II
+// Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+// Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+// Example 1:
+// Input: nums = [3,2,3]
+// Output: [3]
+
+// Example 2:
+// Input: nums = [1]
+// Output: [1]
+
+// Example 3:
+// Input: nums = [1,2]
+// Output: [1,2]
+
+// Constraints:
+// 1 <= nums.length <= 5 * 104
+// -109 <= nums[i] <= 109
+
+// **important question based on moore voting algo and plz see the video ArraysAndStrings1LevelUp for more understanding**
+public class MajorityElements2 {
+    protected boolean isMajority(int[]arr,int num){
+        int count=0;
+        for(Integer e : arr)
+            if(e==num) count++;
+        
+        return count>arr.length/3?true:false;    
+            
+    }
+    
+    public List<Integer> majorityElement(int[] arr) {
+        List<Integer> ans =new ArrayList<>();
+        int val1=arr[0]; //just initializing
+        int val2=arr[0]; //just initializing
+        int counter1=0;
+        int counter2=0;
+        
+        for(int i =0;i<arr.length;i++){
+            if(val1==arr[i])
+                counter1++;
+            else if(val2==arr[i])
+                counter2++;
+            else{
+                if(counter1==0){
+                    val1=arr[i];
+                    counter1++;
+                }
+                else if(counter2==0){
+                    val2=arr[i];
+                    counter2++;
+                }
+                else{
+                    counter1--;
+                    counter2--;
+                }
+            }
+        }
+        if(isMajority(arr,val1))
+            ans.add(val1);
+        
+        if(val2!=val1 && isMajority(arr,val2))
+            ans.add(val2);
+        
+        return ans;    
+    }
+}
---
+++ src/ownPractice/MajorityElement.java
@@ -0,0 +1,42 @@
+package ownPractice;
+//************169. Majority Element
+// Given an array nums of size n, return the majority element.
+// The majority element is the element that appears more than ⌊n / 2⌋ times.
+// You may assume that the majority element always exists in the array.
+
+// Example 1:
+// Input: nums = [3,2,3]
+// Output: 3
+
+// Example 2:
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+ 
+// Constraints:
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -231 <= nums[i] <= 231 - 1
+// Follow-up: Could you solve the problem in linear time and in O(1) space? (Below solution is in linear time and in O(1) space).
+
+// **VERY IMPORTANT QUESTION BASED ON  Boyer-Moore Majority Vote Algorithm and
+// plz see the video ArraysAndStrings1LevelUp for more understanding**
+// **DONE IN O(N) TIME AND O(1) SPACE**
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int maj=nums[0];
+        int counter=1;
+        for(int i=1;i<nums.length;i++){
+            if(maj==nums[i]){
+                counter++;
+            }
+            else if(counter==0){
+                maj=nums[i];
+                counter++;
+            }
+            else counter--;
+        }
+        //if outside of for loop, if counter is 0, it means that there is no such element exist where a 
+        //majority element exist.
+        return maj;
+    }
+}
