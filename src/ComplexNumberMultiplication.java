bd00c4372f7c224941c87655f5148126281196bb
nmn <namanagg.2010@gmail.com>
2021-09-28 01:37:55 +0530

    commit

---
+++ src/ComplexNumberMultiplication.java
@@ -0,0 +1,33 @@
+//***********537. Complex Number Multiplication
+// A complex number can be represented as a string on the form "real+imaginaryi" where:
+// real is the real part and is an integer in the range [-100, 100].
+// imaginary is the imaginary part and is an integer in the range [-100, 100].
+// i2 == -1.
+// Given two complex numbers num1 and num2 as strings, return a string of the complex number that represents their multiplications.
+
+// Example 1:
+// Input: num1 = "1+1i", num2 = "1+1i"
+// Output: "0+2i"
+// Explanation: (1 + i) * (1 + i) = 1 + i2 + 2 * i = 2i, and you need convert it to the form of 0+2i.
+
+// Example 2:
+// Input: num1 = "1+-1i", num2 = "1+-1i"
+// Output: "0+-2i"
+// Explanation: (1 - i) * (1 - i) = 1 + i2 - 2 * i = -2i, and you need convert it to the form of 0+-2i.
+
+// Constraints:
+// num1 and num2 are valid complex numbers.
+public class ComplexNumberMultiplication {
+    public String complexNumberMultiply(String num1, String num2) {
+        int a1 = Integer.parseInt(num1.substring(0, num1.indexOf("+")));
+        int b1 = Integer.parseInt(num1.substring(num1.indexOf("+") + 1, num1.length() - 1));
+
+        int a2 = Integer.parseInt(num2.substring(0, num2.indexOf("+")));
+        int b2 = Integer.parseInt(num2.substring(num2.indexOf("+") + 1, num2.length() - 1));
+
+        int real = a1 * a2 - b1 * b2;
+        int imaginary = a1 * b2 + a2 * b1;
+
+        return real + "+" + imaginary + "i";
+    }
+}
