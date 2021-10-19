13edf000070a1f1976b277009ab62a31fe54fe33
nmn <namanagg.2010@gmail.com>
2021-10-19 18:43:19 +0530

    commit

---
+++ practice.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class practice {
+    static Scanner scn = new Scanner(System.in);
+    public static void main(String[]args){
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        
+    }
+}
