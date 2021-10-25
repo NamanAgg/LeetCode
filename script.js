d7fa9eff2459bda8f7a0b49e9a58f865931ea599
nmn <namanagg.2010@gmail.com>
2021-10-25 17:52:14 +0530

    commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// var x = {
+//     a:5,
+//     b:6
+// };
+
+// console.log(Object.keys(x).length);
+
+// var count=0;
+
+// .parent{
+//     display : flex,
+//     justify-content : center,
+//     margin : auto
+// }
+
+// .child{
+//     display : flex
+// }
+
+// .parent{
+//     position : relative
+// }
+
+// .child{
+//     position : absolute,
+//     transform
+// }
+
+// (function(str){
+// let arr = str.split(" ");
+
+// let maxLength = 0;
+// let word = ""
+// for(let e of arr){
+//     if(maxLength< e.length){
+//         maxLength = e.length;
+//         word = e;
+//     }
+// }
+
+// console.log(maxLength,word);
+
+// })("terminator is less older than neo");
