ea19b055a3930de23503d537ac5c638c2983b18c
nmn <namanagg.2010@gmail.com>
2021-12-21 18:55:51 +0530

    commit

---
+++ src/PowerOfTwo.java
@@ -0,0 +1,37 @@
+//************231. Power of Two
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+// An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+// Example 1:
+// Input: n = 1
+// Output: true
+// Explanation: 20 = 1
+
+// Example 2:
+// Input: n = 16
+// Output: true
+// Explanation: 24 = 16
+
+// Example 3:
+// Input: n = 3
+// Output: false
+
+// Constraints:
+// -231 <= n <= 231 - 1
+
+// Follow up: Could you solve it without loops/recursion?
+public class PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        String num = Integer.toString(n,2);
+        boolean flag = false;
+        for(int i=0;i<num.length();i++){
+            if(num.charAt(i)=='1'){
+                if(flag) return false;
+                else flag = true;
+            }
+        }
+        
+        return true;
+    }
+}
